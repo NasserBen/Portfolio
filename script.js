22613eaae576917250b9b457a99e795bb14afa98
Nasser <nasserbenyed@gmail.com>
2021-05-11 17:10:46 -0700

    fixed nav, cleaned code, finished mobile

--- script.js
+++ script.js
@@ -61,35 +61,31 @@ const navSlide = () => {
   const menuRotate = document.querySelector('.menuRotate')
   const nav = document.querySelector('.nav-links')
   const navLinks = document.querySelectorAll('.nav-links li')
+  let navActive = false;
 
-  burger.addEventListener('click', openNav)
   nav.addEventListener('click', e => {
-   
-    if (e.target == e.currentTarget) closeNav() 
+    if (e.target == e.currentTarget) toggleNav() 
   })
   
-  function openNav () {
+  burger.addEventListener('click', toggleNav)
+
+  function toggleNav () {
     //Animate Burger
+    navActive = !navActive;
     burger.classList.toggle('active')
     menuRotate.classList.toggle('active')
     //Toggle Nav
     nav.classList.toggle('nav-active')
     //Animate Links
-    navLinks.forEach((link, i) => {
-    // Add nav link animations
-    link.style.animation = `navLinkFade 0.5s ease forwards  ${i / 7 + 0.3}s`
-    link.addEventListener('click', closeNav)
-    })
-  }
-
-  function closeNav () {
-    //Reset Animated Burger
-    burger.classList.toggle('active')
-    menuRotate.classList.toggle('active')
-    //Close Nav
-    nav.classList.toggle('nav-active')
-    // Reset Nav Link Animation
-    navLinks.forEach(link => (link.style.animation = ''))
+    if (navActive) {
+      navLinks.forEach((link, i) => {
+        // Add nav link animations
+        link.style.animation = `navLinkFade 0.5s ease forwards  ${i / 7 + 0.3}s`
+        link.addEventListener('click', toggleNav)
+        })
+    } else {
+      navLinks.forEach(link => (link.style.animation = '')) 
+    }
   }
 }
 
